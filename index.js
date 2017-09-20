72a5ab4438465a79b2d5023d27cef2ec538df2b7
jaubourg <j@ubourg.net>
2017-09-20 10:15:13 +0200

    Ports authent token validation from proxy to proxy-internal

--- index.js
+++ index.js
@@ -4,4 +4,5 @@ module.exports = {
     "baseConfig": require( `./lib/baseConfig` ),
     "createProxyServer": require( `./lib/createProxyServer` ),
     "launchers": require( `./lib/launchers` ),
+    "validateAuthent": require( `./lib/validateAuthent` ),
 };
